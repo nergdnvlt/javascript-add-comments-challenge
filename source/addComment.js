50978026bf856b56ac2add6c94a367fbcfeea7fd
nergdvnlt <tylerlundgren@yahoo.com>
2018-06-23 14:22:44 -0600

    Finishing the add comment exercise in JS.

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
-  // add your code here
-})
+  $("form").hide();
+
+  $("#new_comment_button").click(function (){
+    $("#new_comment_button").hide();
+    $("form").show();
+  });
+
+  $("#form_submit").on("click", function(event) {
+    event.preventDefault();
+    $("form").hide();
+    $("#new_comment_button").show();
+
+    let comment = $('input[name="comment"]').val();
+    let author = $('input[name="author"]').val();
+
+    if (comment != "Your comment here...") {
+      $(`<li>${comment}<span class="author">${author}</span></li>`).appendTo("#comment_list");
+    }
+  });
+});
